2bef2440c13e35a5e8a02107da72e5a4a15eac17
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-11-03 17:21:25 -0500

    Verify game & script links

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ class Obstacle {
     this.height = height;
     this.direction = Math.floor(2 * Math.random() - 1);
   }
-  
   moveDownForever() {
     setInterval(() => {
       this.y += 2 * Math.random() + 1.5;
