ce53a85d12b1ad47df647e3dd6afbc6b40198665
CodesGM <54691076+CodesGM@users.noreply.github.com>
2019-11-05 09:16:09 -0500

    Added menu.js file and linked to index.html

---
+++ menu.js
@@ -0,0 +1,18 @@
+app = document.querySelector("#app");
+app.addEventListener("click", function() {
+  var el = document.documentElement,
+    rfs =
+      el.requestFullScreen ||
+      el.webkitRequestFullScreen ||
+      el.mozRequestFullScreen;
+  rfs.call(el);
+});
+function launchFullScreen(element) {
+  if (element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  }
+}
