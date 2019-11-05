83fd532c0ff64ece23827a880419a41d137a34db
Shahriyar <shahryar102@yahoo.com>
2019-11-05 10:37:05 -0500

    modifying game2.js and script2.js

---
+++ game2.js
@@ -0,0 +1,92 @@
+let obstacleArray = [];
+let redArray = [];
+class Hero {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+  }
+}
+/////////////////////////////
+class Obstacle {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.directionX = 2 * Math.round(Math.random()) - 1;
+    this.directionY = 1;
+  }
+  moveDownForever() {
+    setInterval(() => {
+      this.y += .7 * Math.random() * this.directionY;
+      this.x += .7 * Math.random() * this.directionX;
+      if (this.x >= 340) {
+        this.directionX = -1;
+      }
+      if (this.x <= -10) {
+        this.directionX = 1;
+      }
+      if (this.y > 340) {
+        this.directionY = -1;
+      }
+      if (this.y < 0) {
+        this.directionY = 1;
+      }
+      theGame.collisionDetect(theGame.theHero.x, theGame.theHero.y);
+    }, 8);
+  }
+}
+///////////////////////////////////////////////
+class Game {
+  constructor() {
+    this.theHero = new Hero(150, 340, 50, 50);
+    this.score = 0;
+    this.numberOfBalls = 0;
+  }
+  writeScore() {
+    this.score += 10;
+    document.getElementById("score1").innerHTML = `${this.score}`;
+  }
+  spawnObstacle() {
+    let rX = Math.floor(Math.random() * 325);
+    let rY = Math.floor(Math.random() * 1);
+    let rWidth = 80;
+    let rHeight = 60;
+    let newObstacle = new Obstacle(rX, rY, rWidth, rHeight);
+    this.numberOfBalls++;
+    obstacleArray.push(newObstacle);
+    newObstacle.moveDownForever();
+  }
+
+  moveHero(futureX, futureY) {
+    if (
+      futureX + this.theHero.width <= 400 &&
+      futureX >= 0 &&
+      futureY + this.theHero.height <= 400 &&
+      futureY >= 0
+    ) {
+      this.theHero.x = futureX;
+      this.theHero.y = futureY;
+    }
+    if (futureX + this.theHero.width >= 380) {
+      this.theHero.x = futureX;
+      this.theHero.x -= 50;
+    }
+  }
+  collisionDetect(futureX, futureY) {
+    obstacleArray.forEach((obstacle, j) => {
+      if (
+        futureX + 20 + this.theHero.width >= obstacle.x &&
+        futureX + 20 <= obstacle.x + obstacle.width &&
+        futureY + this.theHero.height >= obstacle.y &&
+        futureY <= obstacle.y + obstacle.height  )
+        {
+        return true;
+        } else {
+        return false;
+        }
+    });
+  }
+}
---
+++ script2.js
@@ -0,0 +1,85 @@
+const ctx = document.getElementById("example").getContext("2d");
+//////////////////////////////////////
+const player = new Image();
+player.src = "./images/player.png";
+const redBall = new Image();
+redBall.src = "./images/redball.png";
+const ball = new Image();
+ball.src = "./images/ball.gif";
+const gameOver = new Image();
+gameOver.src = "./images/gameover.jpg";
+function drawPlayer(u) {
+  ctx.drawImage(player, u.x, u.y, 100, 50);
+}
+function drawBall(u, obs) {
+  if (obs) {
+    ctx.drawImage(redBall, u.x, u.y, 90, 120);
+  } else {
+    ctx.drawImage(player, u.x, u.y, 40, 60);
+  }
+}
+
+/////////////////////////////
+let frames = 0;
+function mainLoop() {
+  frames++;
+  //   if (theGame.score <= -300) {
+  //     ctx.drawImage(gameOver, 0, 0, 200, 200);
+  //     return;
+  //   }
+  setTimeout(theGame.writeScore(), theGame.collisionDetect(theGame.theHero.x,theGame.theHero.y),400);
+  if (theGame.collisionDetect(theGame.theHero.x,theGame.theHero.y)){
+    ctx.drawImage(gameOver, 0, 0, 200, 200);
+    return;
+      }
+  ctx.clearRect(0, 0, 400, 400);
+  // this is where we draw the hero
+  drawPlayer(theGame.theHero);
+  // then we draw all the obstacles
+  obstacleArray.forEach(eachObstacle => {
+    drawBall(eachObstacle, true);
+  });
+  /////////////
+  //   redArray.forEach(redBall => {
+  //     drawRedBall(redBall, true);
+  //   });
+  ////////////////////
+  if (theGame.numberOfBalls < 5) {
+    if (frames % 400 === 0) {
+      theGame.spawnObstacle();
+    }
+  }
+
+  //   if (frames % 300 === 0) {
+  //     theGame.spawnRed();
+  //   }
+  requestAnimationFrame(mainLoop);
+}
+let speed = 50;
+document.onkeydown = function(e) {
+  if (e.keyCode == 32) {
+    theGame.moveHero(theGame.theHero.x, 340);
+  }
+  if (e.key === "ArrowUp") {
+    theGame.collisionDetect(theGame.theHero.x, theGame.theHero.y - speed);
+    theGame.moveHero(theGame.theHero.x, theGame.theHero.y - speed);
+  }
+  if (e.key === "ArrowDown") {
+    theGame.collisionDetect(theGame.theHero.x, theGame.theHero.y + speed);
+    theGame.moveHero(theGame.theHero.x, theGame.theHero.y + speed);
+  }
+  if (e.key === "ArrowLeft") {
+    theGame.collisionDetect(theGame.theHero.x - speed, theGame.theHero.y);
+    theGame.moveHero(theGame.theHero.x - speed, theGame.theHero.y);
+  }
+  if (e.key === "ArrowRight") {
+    theGame.collisionDetect(theGame.theHero.x + speed, theGame.theHero.y);
+    theGame.moveHero(theGame.theHero.x + speed, theGame.theHero.y);
+  }
+};
+document.getElementById("start").onclick = startGame;
+let theGame;
+function startGame() {
+  theGame = new Game();
+  mainLoop();
+}
